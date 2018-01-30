42bdc37e7f887271bbb9ac33177547b9e9b52494
Alexis Barltrop <alexisbarltrop@gmail.com>
2018-01-31 11:59:16 +1300

    Does reduction of top term and carries out multiplication to get the answer

--- Etude12_Combinations/Combinations.java
+++ Etude12_Combinations/Combinations.java
@@ -10,29 +10,83 @@ public class Combinations{
         System.err.println("n : " +n + ", k : " + k);
 
         long bottom = n -k;
-        
-       
+        long[] bottomArray;
+	long[] topArray;
+
+	//Generate Arrays
         if (k < bottom){
-            for (long i = n; i >bottom;i--){
-                System.err.print(i+ ", ");
+	    // k less than n-k
+	   
+	    bottomArray = new long[(int)k];//Possible problem with int cast, maybe try araylist.
+	    int bottomArrayCounter = 0;
+	    topArray = new long[(int)k];
+	    int topArrayCounter = 0;
+	    for (long i = n; i >bottom;i--){
+		topArray[topArrayCounter] = i;
+		topArrayCounter++;
+		//System.err.print(i+ ", ");
             }
-            System.err.println();
+	    System.err.println(Arrays.toString(topArray));
+            //System.err.println();
             for( long i= k; i >0; i--){
-                System.err.print(i+ ", ");
+		bottomArray[bottomArrayCounter] = i;
+		bottomArrayCounter++;
+                //System.err.print(i+ ", ");
             }
+	    System.err.println(Arrays.toString(bottomArray));
             
         }else{
+	    // k larger than n-k
+	    bottomArray = new long[(int)bottom];//Possible problem with int cast, maybe try araylist.
+	    int bottomArrayCounter = 0;
+	    topArray = new long[(int)bottom];
+	    int topArrayCounter = 0;
             for (long i = n; i >k;i--){
-                System.err.print(i+ ", ");
+		topArray[topArrayCounter] = i;
+		topArrayCounter++;
+                //System.err.print(i+ ", ");
             }
-            System.err.println();
+	    System.err.println(Arrays.toString(topArray));
+            //System.err.println();
             for( long i = bottom; i>0;i--){
-                System.err.print(i+ ", ");
+		bottomArray[bottomArrayCounter] = i;
+		bottomArrayCounter++;
+                //System.err.print(i+ ", ");
             }
-           
-            
-        }
+	    System.err.println(Arrays.toString(bottomArray));
+	}
+
+	//End of array generation
+
+	//Start reduction
+
+	for (int i = 0; i< topArray.length; i++){
+	    
+	    for(int j = 0; j <bottomArray.length; j++){
+		if (bottomArray[j] != 1){
+		    if (topArray[i]% bottomArray[j] ==0){
+			topArray[i] /= bottomArray[j];
+			bottomArray[j] = 1; 
+		    }
+		}
+	    }
+	}
+	System.err.println(Arrays.toString(topArray));
+
+	//End of reduction
+
+
+	//Final Multiplication
+
+	long output = topArray[0];
+	for( int i = 1; i <topArray.length;i++){
+	    output *= topArray[i];
+	}
+
+	System.out.println("Answer : " + output);
 
     }
+
+     
 
 }

058ff2a86d30e7e111b2c3300d2a02623a57a63b
Alexis Barltrop <alexisbarltrop@gmail.com>
2018-01-30 14:36:43 +1300

    No longer preprocessing dictionary

--- Etude9_JoinedWriting/JoinedWordsApp.java
+++ Etude9_JoinedWriting/JoinedWordsApp.java
@@ -20,29 +20,36 @@ public class JoinedWordsApp{
             Node wordNode = new Node(nextWord);
             dictionary.add(wordNode);
         }
-
+        /*
         for(Node n: dictionary){
             
             findForwards(n,dictionary);
             //findBackwards(n,dictionary);
         }
+        */
         /*
         for (Node n: dictionary){
             System.err.println(n);
             System.err.println("\t" + n.forwards);
         }
         */
+        //This is where we do the search
         if(args.length>0){
             String firstWord = args[0];
             String lastWord = args[1];
             Node firstNode = findNode(firstWord, dictionary);
             Node lastNode = findNode(lastWord, dictionary);
-            System.err.println(search(firstNode, lastNode));
+            System.err.println(searchSingle(firstNode, lastNode));
+            System.err.println(output);
+            System.err.println(searchDouble(firstNode,lastNode));
             System.err.println(output);
         }
-        //This is where we do the search
-        
-
+        /* 
+        for (Node n: dictionary){
+            System.err.println(n);
+            System.err.println("\t" + n.forwards);
+        }
+        */
         
         //For each node find the words that are linked going forward, so words that start with the end of the word.
         //Find words that end with the start of the word.
@@ -51,10 +58,11 @@ public class JoinedWordsApp{
 
     }
 
-    public static boolean search(Node root, Node target){
+    public static boolean searchDouble(Node root, Node target){
         //ArrayList<Node> outputHolder = new ArrayList<Node>();
+        output.add(root);
         for (int depth = 0 ; depth < dictionary.size()/10;depth++){
-            boolean found = DLS(root,target,depth);
+            boolean found = DLSDouble(root,target,depth);
             if(found){
                 return found;
             }
@@ -62,15 +70,55 @@ public class JoinedWordsApp{
         return false;
     }
 
-    public static boolean DLS(Node node, Node target, int depth){
+    public static boolean DLSDouble(Node node, Node target, int depth){
+        
         if(depth == 0 && node.getWord().equals(target.getWord())){
             return true;
         }
         if (depth > 0){
+            //findForwards(node,dictionary);
+            findDoublyLinkedForwards(node,dictionary);
             for(Map.Entry<Node,Integer>  n : node.forwards.entrySet()){
                 output.add(n.getKey());
-                System.err.println(output);
-                boolean found = DLS(n.getKey(),target,depth-1);
+                //This line prints the current state of the search.
+                //System.err.println(output);
+                boolean found = DLSDouble(n.getKey(),target,depth-1);
+                
+                if(found){
+                    return found;
+                }
+                output.remove(n.getKey());
+            }
+        }
+        return false;
+    }
+
+    public static boolean searchSingle(Node root, Node target){
+        //ArrayList<Node> outputHolder = new ArrayList<Node>();
+        output.add(root);
+        for (int depth = 0 ; depth < dictionary.size()/10;depth++){
+            boolean found = DLSSingle(root,target,depth);
+            if(found){
+                return found;
+            }
+        }
+        return false;
+    }
+
+    public static boolean DLSSingle(Node node, Node target, int depth){
+        
+        if(depth == 0 && node.getWord().equals(target.getWord())){
+            return true;
+        }
+        if (depth > 0){
+            //findForwards(node,dictionary);
+            findSinglyLinkedForwards(node,dictionary);
+            //findDoublyLinkedForwards(node,dictionary);
+            for(Map.Entry<Node,Integer>  n : node.forwards.entrySet()){
+                output.add(n.getKey());
+                //This line prints the current state of the search.
+                //System.err.println(output);
+                boolean found = DLSSingle(n.getKey(),target,depth-1);
                 
                 if(found){
                     return found;
@@ -91,6 +139,48 @@ public class JoinedWordsApp{
 
     }
 
+    public static void findSinglyLinkedForwards(Node node, ArrayList<Node> dictionary){
+        String word = node.getWord();
+        for(Node n : dictionary){
+            String otherWord = n.getWord();
+            if(!(word.equals(otherWord))){
+                for (int i = 0; i < word.length(); i++){
+                    if(otherWord.length() >=word.length()-i){ // Checks that other word is long enough
+                        if(word.substring(i)
+                           .equals(otherWord.substring(0,word.length()-i))){
+                            if(word.substring(i).length() >= (int)Math.ceil(word.length() /2.0)
+                               || word.substring(i).length() >= (int) Math.ceil(otherWord.length()/2.0)){
+                                //System.err.println("Check in double linked loop");
+                                node.addForwards(n,word.length()-i);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public static void findDoublyLinkedForwards(Node node, ArrayList<Node> dictionary){
+        String word = node.getWord();
+        for(Node n : dictionary){
+            String otherWord = n.getWord();
+            if(!(word.equals(otherWord))){
+                for (int i = 0; i < word.length(); i++){
+                    if(otherWord.length() >=word.length()-i){ // Checks that other word is long enough
+                        if(word.substring(i)
+                           .equals(otherWord.substring(0,word.length()-i))){
+                            if(word.substring(i).length() >= (int)Math.ceil(word.length() /2.0)
+                               && word.substring(i).length() >= (int) Math.ceil(otherWord.length()/2.0)){
+                                //System.err.println("Check in double linked loop");
+                                node.addForwards(n,word.length()-i);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
     public static void findForwards(Node node, ArrayList<Node> dictionary){
         String word = node.getWord();
         for(Node n: dictionary){
